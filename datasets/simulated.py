467ac5831c0a8de4069e3f5fc1a2ced30758d364
mathurinm <mathurin.massias@gmail.com>
2022-01-24 16:04:08 +0100

    use make_correlated_data in simulated

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -2,6 +2,7 @@ from benchopt import BaseDataset, safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
+    from benchopt.datasets import make_correlated_data
 
 
 class Dataset(BaseDataset):
@@ -10,8 +11,8 @@ class Dataset(BaseDataset):
 
     parameters = {
         'n_samples, n_features': [
-            (100, 5000),
-            (100, 10000)]
+            (300, 1000),
+            (1000, 300)]
     }
 
     def __init__(self, n_samples=10, n_features=50, random_state=42):
@@ -20,9 +21,11 @@ class Dataset(BaseDataset):
         self.random_state = random_state
 
     def get_data(self):
-        rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = 2*(rng.randn(self.n_samples) > 0) - 1
+        X, y, _ = make_correlated_data(
+            n_samples=self.n_samples, n_features=self.n_features,
+            random_state=self.random_state
+        )
+        y = 2*(y > 0) - 1
 
         data = dict(X=X, y=y)
 
